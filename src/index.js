821d97006c0bc1903e95977a3730ae9162137012
betterwork7777 <kiontroy@gmail.com>
2025-07-12 01:37:22 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import N400Form from './N400Form';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <N400Form />
+  </React.StrictMode>
+);
